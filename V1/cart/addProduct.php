0372cc33efdfadf04ba076080596925759cc92e0
AnnaNordberg <56160770+AnnaNordberg@users.noreply.github.com>
2020-03-29 19:56:58 +0200

    fixed delete products and update products

--- V1/cart/addProduct.php
+++ V1/cart/addProduct.php
@@ -1,23 +1,23 @@
 <?php
 include("../objects/Products.php");
-$posts_object = new Posts($databaseHandler);
+$products_object = new Products($databaseHandler);
 
-$title_IN = ( isset($_GET['name']) ? $_GET['name'] : '' );
-//$content_IN = ( isset($_GET['content']) ? $_GET['content'] : '' );
+$title_IN = ( isset($_GET['productName']) ? $_GET['productName'] : '' );
+// $content_IN = ( isset($_GET['content']) ? $_GET['content'] : '' );
 
 
-/* if(!empty($title_IN)) {
-    if(!empty($content_IN)) {
+ if(!empty($title_IN)) {
+  // if(!empty($content_IN)) {
+ // tog bort  $products_object->addproduct($title_IN, >>>>$content_IN<<<<<<); ur raden nedan
+       $products_object->addProduct($title_IN);
 
-        $posts_object->addPost($title_IN, $content_IN);
-
-    } else {
-        echo "Error: content cannot be empty!";
-    }
-} else {
+   // } else {
+   /*     echo "Error: content cannot be empty!";
+    }  */
+ } else {
     echo "Error: titel cannot be empty!";
-}
+ }
 
- */
+
 
 ?>
--- V1/objects/Products.php
+++ V1/objects/Products.php
@@ -2,9 +2,9 @@
 
 include("../../config/database_handler.php");
 
-class Posts {
+class Products {
     private $database_handler;
-    private $post_id;
+    private $product_id;
 
     public function __construct( $database_handler_IN ) {
 
@@ -12,20 +12,24 @@ class Posts {
 
     }
 
-    public function setPostId($post_id_IN) {
+    public function setProductId($product_id_IN) {
 
-        $this->post_id = $post_id_IN;
+        $this->product_id = $product_id_IN;
 
     }
 
-    public function fetchSinglePost() {
+    public function fetchSingleProduct() {
 
-        $query_string = "SELECT ID, name, price, availability FROM Products WHERE ID=:post_id";
+        //Ändrade WHERE id=:product_id till ID
+        //kan vara problem med namnet availability?
+        
+
+        $query_string = "SELECT ID, productName, price, availability FROM Products WHERE ID=:product_id";
         $statementHandler = $this->database_handler->prepare($query_string);
 
         if($statementHandler !== false) {
             
-            $statementHandler->bindParam(":post_id", $this->post_id);
+            $statementHandler->bindParam(":product_id", $this->product_id);
             $statementHandler->execute();
 
             return $statementHandler->fetch();
@@ -38,9 +42,9 @@ class Posts {
         }
     }
 
-    public function fetchAllPosts() {
+    public function fetchAllProducts() {
 
-        $query_string = "SELECT ID, name, price, availability FROM Products";
+        $query_string = "SELECT ID, productName, price, availability FROM Products";
         $statementHandler = $this->database_handler->prepare($query_string);
 
         if($statementHandler !== false) {
@@ -55,22 +59,23 @@ class Posts {
         
     }
 
-     public function addPost($title_param) {
+    //Ändrade om namnen i :name_IN och tog även bort ,content_param från addproduct( ).
+    public function addProduct($title_param) {
 
-        $query_string = "INSERT INTO Products (name, price, availability) VALUES(:name_IN, 100, 20)";
+        $query_string = "INSERT INTO Products (productName, price, availability) VALUES(:name_IN, 150, 20)";
         $statementHandler = $this->database_handler->prepare($query_string);
 
         if($statementHandler !== false) {
 
             $statementHandler->bindParam(":name_IN", $title_param);
-            //$statementHandler->bindParam(":price_IN", $content_param);
+           /*  $statementHandler->bindParam(":content_IN", $content_param); */
             
             $success = $statementHandler->execute();
 
             if($success === true) {
                 echo "OK!";
             } else {
-                echo "Error while trying to insert post to database!";
+                echo "Error while trying to insert product to database!";
             }
 
         } else {
@@ -80,36 +85,68 @@ class Posts {
     } 
 
 
-//testar byta title till name
-    public function updatePost($data) {
 
+    public function updateProduct($data) {
 
-        if(!empty($data['name'])) {
-            $query_string = "UPDATE Products SET name=:name WHERE ID=:post_id";
+        // Testar att byta title till name -- if(!empty($data['title'])) 
+
+        if(!empty($data['productName'])) {
+            $query_string = "UPDATE Products SET productName=:productName WHERE ID=:product_id";
             $statementHandler = $this->database_handler->prepare($query_string);
 
-            $statementHandler->bindParam(":name", $data['name']);
-            $statementHandler->bindParam(":post_id", $data['ID']);
+            $statementHandler->bindParam(":productName", $data['productName']);
+            $statementHandler->bindParam(":product_id", $data['ID']);
 
             $statementHandler->execute();
             
         }
 
+        // Testar att byta content till availability , 
+        //Ändrade även [id] till [Id]
+
         if(!empty($data['availability'])) {
-            $query_string = "UPDATE Products SET availability=:availability WHERE ID=:post_id";
+            $query_string = "UPDATE Products SET availability=:availability WHERE ID=:product_id";
             $statementHandler = $this->database_handler->prepare($query_string);
 
             $statementHandler->bindParam(":availability", $data['availability']);
-            $statementHandler->bindParam(":post_id", $data['ID']);
+            $statementHandler->bindParam(":product_id", $data['ID']);
 
             $statementHandler->execute();
             
         }
 
-        $query_string = "SELECT ID, name, price, availability FROM Products WHERE ID=:post_id";
+        $query_string = "SELECT ID, productName, price, availability FROM Products WHERE ID=:product_id";
         $statementHandler = $this->database_handler->prepare($query_string);
 
-        $statementHandler->bindParam(":post_id", $data['ID']);
+        $statementHandler->bindParam(":product_id", $data['ID']);
+        $statementHandler->execute();
+
+        echo json_encode($statementHandler->fetch());
+
+
+    }
+
+
+    // Ett försök att skapa DELETEproduct funktion
+
+    public function deleteProduct($data) {
+
+
+        if(!empty($data['ID'])) {
+            $query_string = "DELETE FROM Products WHERE ID=:product_id";
+            $statementHandler = $this->database_handler->prepare($query_string);
+
+            $statementHandler->bindParam(":product_id", $data['ID']);
+
+            $statementHandler->execute();
+            
+        }
+
+    
+        $query_string = "SELECT ID FROM Products WHERE ID=:product_id";
+        $statementHandler = $this->database_handler->prepare($query_string);
+
+        $statementHandler->bindParam(":product_id", $data['ID']);
         $statementHandler->execute();
 
         echo json_encode($statementHandler->fetch());
